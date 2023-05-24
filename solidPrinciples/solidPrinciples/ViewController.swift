d5fb68cea1c8309a9591df0daab77caeadba080e
Leonardo Mendes <leonardomendes@Leonardos-MacBook-Pro.local>
2023-05-24 16:23:05 -0300

    setup initial project

---
+++ solidPrinciples/solidPrinciples/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  solidPrinciples
+//
+//  Created by Leonardo Mendes on 24/05/23.
+//  Copyright © 2023 Leonardo Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
